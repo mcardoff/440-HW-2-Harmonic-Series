bcb049ed0fe876e55bfbebad5fe3cd9565a36efc
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-03 21:59:35 -0600

    Initial Commit

---
+++ Shared/HW_2_Harmonic_SeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_2_Harmonic_SeriesApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HW_2_Harmonic_SeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
